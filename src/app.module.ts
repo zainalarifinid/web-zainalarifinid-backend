d6cd7b92e2945282be4976e2b0422f1a30b6f71b
Zainal Arifin <arifin.1602@gmail.com>
2019-08-09 00:26:01 +0700

    feat(docker) setup docker compose for backend project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NoteModule } from './Notes';
 
 @Module({
-  imports: [],
+  imports: [
+    NoteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
